991586477271f9636b9969308afad9e79aa3f9be
rbashenskyi <bashenskii@gmail.com>
2021-09-15 18:21:23 +0300

    DEV-2: added temp sql queries

--- src/main/java/TestProject.java
+++ src/main/java/TestProject.java
@@ -1,5 +1,5 @@
 public class TestProject {
     public static void main(String[] args) {
-        System.out.print("Hello Ruslan!");
+        System.out.print("Hello Volodia!");
     }
 }
---
+++ src/main/resources/demo.sql
@@ -0,0 +1,3 @@
+SELECT * FROM devices;
+
+SELECT * FROM temp;
